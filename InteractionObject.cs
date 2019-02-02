2a42c28ebfb861dd5d24755ac5ad777104d9c95a
LaurenStamp <47026726+LaurenStamp@users.noreply.github.com>
2019-02-02 11:32:12 +1100

    Interactable Object Unity C#
    Used for Impaired by Lauren Stamp - for items such as doors, keys and medkits

---
+++ InteractionObject.cs
@@ -0,0 +1,40 @@
+﻿// This script is used for Impaired by Lauren Stamp
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionObject : MonoBehaviour
+{
+    // checks if item can be put it inventory, if it is openable, if it is locked, or if it is a medkit
+	public bool inventory;
+	public bool openable;
+	public bool locked, isKey;
+	public bool isMedkit;
+	public GameObject itemNeeded;
+
+	public Animator anim;
+
+    void Update()
+    {
+    }
+
+	public void DoInteraction()
+	{
+		gameObject.SetActive (false);
+	}
+
+	public void Open()
+	{
+		anim.SetBool ("open", true);
+	}
+    // checks if item is a key
+    public bool IsKey()
+    {
+        return isKey;
+    }
+    // checks if item is a medkit
+    public bool IsMedkit()
+    {
+        return isMedkit;
+    }
+}
